3fcef63eb3e9f06a87d96fdcb2e95f96e6333ec7
RasAlex <magvaj@gmail.com>
2019-01-20 17:02:29 +0200

    Initial commit

---
+++ src/ru/geekbrains/guess_the_number/Main.java
@@ -0,0 +1,31 @@
+package ru.geekbrains.guess_the_number;
+
+import java.awt.desktop.SystemEventListener;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ваша задача угадать число. Удачи!");
+        int range = 20;
+        int number = (int)(Math.random()*range);
+        while (true)
+        {
+            System.out.println("Угадайте число от 0 до " + range);
+            int input_number = scanner.nextInt();
+            if (input_number == number)
+            {
+                System.out.println("Угадали!");
+                break;
+            }else if (input_number > number)
+            {
+                System.out.println("Загаданное число меньше!");
+            } else
+            {
+                System.out.println("Загаданное число больше");
+            }
+        }
+        scanner.close();
+    }
+}
